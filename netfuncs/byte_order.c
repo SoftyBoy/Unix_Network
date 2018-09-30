8229e4952a3fb0f8e9242244bc8e61d6ddf1dc1c
SoftyBoy <cjw164258@163.com>
2018-09-30 11:05:22 +0800

    add netfuncs directory and relative functions

---
+++ netfuncs/byte_order.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
+#undef CPU_VENDOR_OS
+#define CPU_VENDOR_OS "$host"
+
+
+int main(const int argc, const char *argv[])
+{
+    union {
+		short s;
+		char c[sizeof(short)];
+	}un;
+
+	un.s = 0x0102;
+	printf("%s: ", CPU_VENDOR_OS);
+    
+	if(2 == sizeof(short)) 
+	{	
+		if (un.c[0] == 1 && un.c[1] == 2) {
+			printf("big-endian\n");
+		}
+		else if (un.c[0]==2 && un.c[1] == 1) {
+			printf("little-endian\n");
+		}
+		else {
+			printf("unknown\n");
+		}
+	}
+	else
+	{
+		printf("sizeof(short)=%lu\n", sizeof(short));
+	}
+
+    return 0;
+}
